98930ef41c769f29bc91cb7d681599663938587a
Anders Lundgren <lundgren.am@gmail.com>
2015-10-29 15:09:09 -0600

    Some changes to bash scripts and configs

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 #java -ea -Xmx4g -cp "openjava.jar:bin/" -Djava.library.path="openjava.jar:/Users/anders/mu_bbmap/src/bbmap/jni" mujava.gui.GenMutantsMain
 
-java -ea -Xmx4g -cp "openjava.jar:bin/:classes/" -Djava.library.path="openjava.jar:/Users/anders/mu_bbmap/src/bbmap/jni" mujava.gui.GenMutantsMain
+java -ea -Xmx4g -cp "/Users/anders/mu_bbmap/openjava.jar:/Users/anders/mu_bbmap/bin/:/Users/anders/mu_bbmap/classes/" -Djava.library.path="openjava.jar:/Users/anders/mu_bbmap/src/bbmap/jni" mujava.gui.GenMutantsMain
--- testnew.sh
+++ testnew.sh
@@ -1,2 +1,2 @@
 rm -r script_session;
-java -cp "openjava.jar:commons-io-2.4.jar:bin/:classes/" mujava.cli.testnew script_session bbmap/current/align2/BBmap.java;
+java -cp "openjava.jar:commons-io-2.4.jar:bin/:classes/" mujava.cli.testnew script_session bbmap/current/align2/BBMap.java;
