b7852c01220e0a104a7d18080b8aea6798631a95
Anders Lundgren <lundgren.am@gmail.com>
2015-11-02 13:57:27 -0700

    mutant.rb can create alterantives for source lines now

--- mutator/generate_faults.rb
+++ mutator/generate_faults.rb
@@ -1,14 +1,17 @@
 require 'optparse'
+require 'fileutils'
+
+require_relative 'mutant'
+
+@here = File.dirname(__FILE__)
+@results_dir = File.join( @here, "..", "result")
+@package = "bbmap.current.align2"
 
 #Reformat the source with eclipse
 #Scan the source file
     #make new copies of each faulty version and keep them in subdirectories here, not in the original source location
 #Make class files using -cp ORIGINAL_LOCATION
 #Move all the new source and binary files to the appropriate directory in /results/ so muJava can work with them
-
-
-
-
 
 OptionParser.new do |options|
     options.banner = "Usage: generate_faults.rb <file1> <file2> ... [options]"
@@ -21,38 +24,40 @@ p ARGV
 #%x(~/eclipse/eclipse -application org.eclipse.jdt.core.JavaCodeFormatter -config ~/mu_bbmap/mutator/java_format.prefs ~/mu_bbmap/mutator/src/BBMap.java)
 
 def parse_dir(dir)
-    print dir
     Dir.foreach(dir) do |file|
         parse_file File.join(dir, file) unless ((file == ".") or (file == ".."))
     end
 end
 
-def parse_file(file)
+def parse_file(full_path)
+
+    filename = File.split(full_path)[-1]
+    puts "Parsing #{filename}"
+
+    #ensure the correct directory structure exists for the results
+    new_src_dir = File.join( @results_dir, "#{@package}.#{filename[0..-6]}") #strip ".java" from file
+    FileUtils::mkdir_p File.join(new_src_dir, "original")
+    FileUtils::mkdir_p File.join(new_src_dir, "traditional_mutants")
+
+    File.open(full_path, "r") do |source|
+
+        mutator = Mutant.new
+        original_lines = []
+        mutant_lines = []
+        source.each do |line| #build the arrays
+            original_lines << line
+            mutant_lines << mutator.mutate_line(line)
+        end
+        
+        puts "Original source was #{original_lines.length} lines long"
+        puts "New lines is an array of #{mutant_lines.length} maps"
+        (0...mutant_lines.length).each do |index|
+            new_dirs = mutant_lines[index].keys
+        end
+
+    end
 end
 
-class Mutator
 
-    def initialize
-    Mutator(token, alts)
+parse_dir(File.join(@here, "src"))
 
-end
-
-#implmentable
-#AORB - arithmetic operator replacement binary
-#AORU - arithmetic operator replacement unary
-#AORS - arithmetic operator replacement shortcut
-#AODU - arithmetic operator deletion unary
-#AODS - arithmetic operator deletion shortcut (++, -- only?)
-#ROR - relational operator replacement
-#COR - conditional operator replacement
-#COD - conditional operator deletion (unary)
-#SOR - shift operator replacement
-#LOR - logical operator replacement (binary)
-#LOD - logical operator delete (unary)
-#ASRS - shortcut assignment operator replacement
-
-#non-implementable
-#AOIU - arithmetic operator insertion unary
-#AOIS - arithmetic operator insertion shortcut
-#COI - conditional operator insertion
-#LOI - logical operator insertion
--- mutator/mutant.rb
+++ mutator/mutant.rb
@@ -21,21 +21,78 @@
 class Mutant
 
     attr_reader :mutants
+    attr_reader :types
 
     def initialize()
         @mutants = {
-            "AORB" => ['+', '-', '*', '/', '%'],
-	    "AORU" => ['-', '++', '--'],
-            "AORS" => ['+=', '-=', '*=', '/=', '%=', '>>=', '<<=', '&=', '^='],
-            "AODU" => ['-', '++', '--'],
+            "AORB" => [' + ', ' - ', ' * ', ' / ', ' % '], #binary only
+	    "AORU" => ['++', '--'], #unary only, not including -
+            "AORS" => [' += ', ' -= ', ' *= ', ' /= ', ' %= ', ' >>= ', ' <<= ', ' &= ', ' ^= '],
+            "AODU" => ['++', '--'], #unary only, not including -
             "AODS" => ['<<', '>>', '>>>'],
-            "ROR" => ['>', '>=', '<', '<=', '==', '!='],
-            "COD" => ['!'],#unary only
-            "COR" => ['&&', '||', '&', '|', '^'],#binary only
-            "SOR" => ['<<', '>>', '>>>'],
-            "LOR" => ['&', '|'],#binary only
-            "LOD" => ['~']#unary only
+            "ROR"  => [' > ', ' >= ', ' < ', ' <= ', ' == ', ' != '],
+            "COD"  => ['!'], #unary only
+            "COR"  => [' && ', ' || ', ' & ', ' | ', ' ^ '], #binary only
+            "SOR"  => [' << ', ' >> ', ' >>> '],
+            "LOR"  => [' & ', ' | '], #binary only
+            "LOD"  => ['~'] #unary only
         }
+
+        @types = {
+            "AORB" => "replacement",
+	    "AORU" => "replacement",
+            "AORS" => "replacement",
+            "ROR"  => "replacement",
+            "COR"  => "replacement",
+            "SOR"  => "replacement",
+            "LOR"  => "replacement",
+            "LOD"  => "deletion",
+            "AODU" => "deletion",
+            "AODS" => "deletion",
+            "COD"  => "deletion"
+        }
+
+        @key_counts = {}
+        @mutants.keys.each do |key|
+            @key_counts[key] = 1
+        end
+
+    end
+
+    #produce a map of alternatives given a line of source code
+    def mutate_line(line)
+        new_lines = {}
+        @mutants.keys.each do |key|
+            mutation_type = @types[key]
+            tokens = @mutants[key]
+            tokens.each do |token|
+                offset = 0
+                index = line.index(token, offset)
+                while (!index.nil?)
+                    #this token should produce new mutants on this line, one per other token
+
+                    if mutation_type == "replacement" #many possible mutants
+                        alts = tokens.clone.keep_if { |t| t != token }
+                        alts.each do |alt|
+                            mutant_name = "#{key}_#{@key_counts[key]}"
+                            @key_counts[key] += 1
+                            new_line = "#{line[0...index]}#{alt}#{line[(index + token.length)..-1]}"
+                            new_lines[mutant_name] = new_line
+                        end
+                    elsif mutation_type == "deletion" #one possible mutant
+                        mutant_name = "#{key}_#{@key_counts[key]}"
+                        @key_counts[key] += 1
+                        new_line = "#{line[0...index]}#{line[(index + token.length)..-1]}"
+                        new_lines[mutant_name] = new_line
+                    else
+                        print "Unkown mutation type: #{mutation_type}\n"
+                    end
+                    offset = index + 1
+                    index = line.index(token, offset) #next occurance of the token
+                end
+            end
+        end
+        new_lines
     end
 
 end
